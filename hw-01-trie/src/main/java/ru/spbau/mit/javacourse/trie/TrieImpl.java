12c5d4790422c785b168e7dba59cb439b1fe96ed
alexvangogen <alexvangogen@gmail.com>
2018-03-07 13:52:55 +0300

    Add implementation class with empty methods

---
+++ hw-01-trie/src/main/java/ru/spbau/mit/javacourse/trie/TrieImpl.java
@@ -0,0 +1,29 @@
+package ru.spbau.mit.javacourse.trie;
+
+public class TrieImpl implements Trie {
+
+    @Override
+    public boolean add(String element) {
+        return false;
+    }
+
+    @Override
+    public boolean contains(String element) {
+        return false;
+    }
+
+    @Override
+    public boolean remove(String element) {
+        return false;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public int howManyStartsWithPrefix(String prefix) {
+        return 0;
+    }
+}
