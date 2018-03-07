48f95b7efaa95179d7b73b36c182adf1ad4e64eb
alexvangogen <alexvangogen@gmail.com>
2018-03-07 14:22:30 +0300

    Add trie node implementation

--- hw-01-trie/src/main/java/ru/spbau/mit/javacourse/trie/TrieImpl.java
+++ hw-01-trie/src/main/java/ru/spbau/mit/javacourse/trie/TrieImpl.java
@@ -2,6 +2,8 @@ package ru.spbau.mit.javacourse.trie;
 
 public class TrieImpl implements Trie {
 
+    public static final int ALPHABET_SIZE = 52;
+
     @Override
     public boolean add(String element) {
         return false;
@@ -25,5 +27,71 @@ public class TrieImpl implements Trie {
     @Override
     public int howManyStartsWithPrefix(String prefix) {
         return 0;
+    }
+
+
+    private final class Node {
+
+        public Node() {
+            next = new Node[ALPHABET_SIZE];
+            isTerminal = false;
+            wordsAfter = 1;
+        }
+
+        public Node findByChar(final char ch) throws IllegalArgumentException {
+            final int mappedValue = mapCharToInt(ch);
+            return next[mappedValue];
+        }
+
+        public Node insertByChar(final char ch) throws IllegalArgumentException, UnsupportedOperationException {
+            final int mappedValue = mapCharToInt(ch);
+            if (next[mappedValue].isPresent()) {
+                throw new UnsupportedOperationException("Insertion to the present node is disallowed");
+            } else {
+                next[mappedValue] = new Node();
+                return next[mappedValue];
+            }
+        }
+
+        public boolean isTerminal() {
+            return isTerminal;
+        }
+
+        public boolean isPresent() {
+            return wordsAfter > 0;
+        }
+
+        public void setTerminal() {
+            isTerminal = true;
+        }
+
+        public void resetTerminal() {
+            isTerminal = false;
+        }
+
+        public int getWordsAfter() {
+            return wordsAfter;
+        }
+
+        public void increaseWordsAfter() {
+            wordsAfter++;
+        }
+
+        public void decreaseWordsAfter() {
+            wordsAfter--;
+        }
+
+        private int mapCharToInt(final char ch) throws IllegalArgumentException {
+            if (ch >= 'a' && ch <= 'z') {
+                return ch - 'a';
+            } else if (ch >= 'A' && ch <= 'Z') {
+                return ch - 'A' + (ALPHABET_SIZE >> 1);
+            }
+            throw new IllegalArgumentException("Character must be an alphabetic value");
+        }
+
+        private final Node[] next;
+        private boolean isTerminal;
+        private int wordsAfter;
     }
 }
