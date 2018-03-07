d111b790d309ed2796f48fd65a96151c317b3d40
alexvangogen <alexvangogen@gmail.com>
2018-03-07 13:54:03 +0300

    Add test class for public methods

---
+++ hw-01-trie/src/test/java/ru/spbau/mit/javacourse/trie/TrieTest.java
@@ -0,0 +1,37 @@
+package ru.spbau.mit.javacourse.trie;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+
+public class TrieTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void add() {
+    }
+
+    @Test
+    void contains() {
+    }
+
+    @Test
+    void remove() {
+    }
+
+    @Test
+    void size() {
+    }
+
+    @Test
+    void howManyStartsWithPrefix() {
+    }
+}
