d3fe94a7f446aabab5d4d544817d145948fb517a
alexvangogen <alexvangogen@gmail.com>
2018-03-07 22:43:25 +0300

    Implement tests for "remove" method

--- hw-01-trie/src/test/java/ru/spbau/mit/javacourse/trie/TrieTest.java
+++ hw-01-trie/src/test/java/ru/spbau/mit/javacourse/trie/TrieTest.java
@@ -36,8 +36,8 @@ public class TrieTest {
                 () -> assertFalse(trie.add("z"), "Word \"Z\" already exists"),
                 () -> assertTrue(trie.add(""), "Empty word"),
                 () -> assertTrue(trie.add("A"), "New word as \"A\" != \"a\""),
-                () -> assertTrue(trie.add("Aba"), "\"Aba\" != \"aba\""),
                 () -> assertTrue(trie.add("Abacaba"), "\"Abacaba\" != \"abacaba\""),
+                () -> assertTrue(trie.add("Aba"), "\"Aba\" != \"aba\""),
                 () -> assertTrue(trie.add("ABACABA"), "\"ABACABA\" != \"abacaba\""),
                 () -> assertFalse(trie.add(""), "Empty word already exists"),
                 () -> assertFalse(trie.add("Aba"), "Word \"Aba\" already exists")
@@ -55,8 +55,8 @@ public class TrieTest {
         trie.add("Z");
         trie.add("");
         trie.add("A");
-        trie.add("Aba");
         trie.add("Abacaba");
+        trie.add("Aba");
         trie.add("ABACABA");
         assertAll(
                 "Trie must contain all words inserted above",
@@ -68,8 +68,8 @@ public class TrieTest {
                 () -> assertTrue(trie.contains("Z")),
                 () -> assertTrue(trie.contains("")),
                 () -> assertTrue(trie.contains("A")),
-                () -> assertTrue(trie.contains("Aba")),
                 () -> assertTrue(trie.contains("Abacaba")),
+                () -> assertTrue(trie.contains("Aba")),
                 () -> assertTrue(trie.contains("ABACABA"))
         );
         assertAll(
@@ -82,14 +82,68 @@ public class TrieTest {
                 () -> assertFalse(trie.contains("Y")),
                 () -> assertFalse(trie.contains("yay")),
                 () -> assertFalse(trie.contains("B")),
-                () -> assertFalse(trie.contains("Bcb")),
                 () -> assertFalse(trie.contains("Bcbdbcb")),
+                () -> assertFalse(trie.contains("Bcb")),
                 () -> assertFalse(trie.contains("BCBDBCB"))
         );
     }
 
     @Test
+    @DisplayName("Test method TrieImpl.remove")
     void remove() {
+        trie.add("a");
+        trie.add("aba");
+        trie.add("abacaba");
+        trie.add("zbacaba");
+        trie.add("z");
+        trie.add("Z");
+        trie.add("");
+        trie.add("A");
+        trie.add("Abacaba");
+        trie.add("Aba");
+        trie.add("ABACABA");
+        assertAll(
+                "Trie must successfully remove all words inserted above",
+                () -> assertTrue(trie.remove("a")),
+                () -> assertTrue(trie.remove("aba")),
+                () -> assertTrue(trie.remove("abacaba")),
+                () -> assertTrue(trie.remove("zbacaba")),
+                () -> assertTrue(trie.remove("z")),
+                () -> assertTrue(trie.remove("Z")),
+                () -> assertTrue(trie.remove("")),
+                () -> assertTrue(trie.remove("A")),
+                () -> assertTrue(trie.remove("Abacaba")),
+                () -> assertTrue(trie.remove("Aba")),
+                () -> assertTrue(trie.remove("ABACABA"))
+        );
+        assertAll(
+                "Trie must not contain deleted elements",
+                () -> assertFalse(trie.contains("a")),
+                () -> assertFalse(trie.contains("aba")),
+                () -> assertFalse(trie.contains("abacaba")),
+                () -> assertFalse(trie.contains("zbacaba")),
+                () -> assertFalse(trie.contains("z")),
+                () -> assertFalse(trie.contains("Z")),
+                () -> assertFalse(trie.contains("")),
+                () -> assertFalse(trie.contains("A")),
+                () -> assertFalse(trie.contains("Abacaba")),
+                () -> assertFalse(trie.contains("Aba")),
+                () -> assertFalse(trie.contains("ABACABA"))
+        );
+        assertAll(
+                "\"remove\" must return false when deleting not existing elements",
+                () -> assertFalse(trie.remove("a")),
+                () -> assertFalse(trie.remove("aba")),
+                () -> assertFalse(trie.remove("abacaba")),
+                () -> assertFalse(trie.remove("zbacaba")),
+                () -> assertFalse(trie.remove("z")),
+                () -> assertFalse(trie.remove("Z")),
+                () -> assertFalse(trie.remove("")),
+                () -> assertFalse(trie.remove("A")),
+                () -> assertFalse(trie.remove("Abacaba")),
+                () -> assertFalse(trie.remove("Aba")),
+                () -> assertFalse(trie.remove("ABACABA"))
+        );
     }
 
     @Test
